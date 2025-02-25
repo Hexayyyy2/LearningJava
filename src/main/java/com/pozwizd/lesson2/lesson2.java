582cdae31122948befab8ef264ecf83c0cdf24c8
bleid <bleidbro@gmail.com>
2025-02-25 19:01:48 +0300

    Completed lesson2.md

--- src/main/java/com/pozwizd/lesson2/lesson2.java
+++ src/main/java/com/pozwizd/lesson2/lesson2.java
@@ -4,20 +4,38 @@ import java.util.Scanner;
 
 public class lesson2 {
 
+    static int a = 20;
+    static int b = 10;
+    static double c = 3.14, d = 6.6;
+
     public static void main(String[] args) {
-
-        System.out.println();
+        System.out.println("Целое число: ");
+        System.out.println(summ(a, b) + "\n" + minus(a,b) + "\n" + multiply(a,b) + "\n" + divide(a,b));
+        System.out.println("C плавающей точкой: ");
+        System.out.println(summ(c,d) + "\n" + minus(c,d) + "\n" + multiply(c,d) + "\n" + divide(c,d));
     }
-    public static void metods(int a, int b) {
-
-        int slo = a + b;
-        int vil = a - b;
-        int mno = a * b;
-        int del = a / b;
-
-        double slo_ = a + b;
-        double vil_ = a - b;
-        double mno_ = a * b;
-        double del_ = a / b;
+    public static int summ(int a, int b) {
+        return a + b;
+    }
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+    public static double summ(double a, double b) {
+        return a + b;
+    }
+    public static double minus(double a, double b) {
+        return a - b;
+    }
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+    public static double divide(double a, double b) {
+        return a / b;
     }
 }

d757a5fec502fe342d225015e2667e15eee30e97
bleid <bleidbro@gmail.com>
2025-02-25 19:02:43 +0300

    Completed lesson2.md

--- src/main/java/com/pozwizd/lesson2/lesson2.java
+++ src/main/java/com/pozwizd/lesson2/lesson2.java
@@ -1,7 +1,5 @@
 package com.pozwizd.lesson2;
 
-import java.util.Scanner;
-
 public class lesson2 {
 
     static int a = 20;
@@ -10,11 +8,11 @@ public class lesson2 {
 
     public static void main(String[] args) {
         System.out.println("Целое число: ");
-        System.out.println(summ(a, b) + "\n" + minus(a,b) + "\n" + multiply(a,b) + "\n" + divide(a,b));
+        System.out.println(suma(a, b) + "\n" + minus(a,b) + "\n" + multiply(a,b) + "\n" + divide(a,b));
         System.out.println("C плавающей точкой: ");
-        System.out.println(summ(c,d) + "\n" + minus(c,d) + "\n" + multiply(c,d) + "\n" + divide(c,d));
+        System.out.println(suma(c,d) + "\n" + minus(c,d) + "\n" + multiply(c,d) + "\n" + divide(c,d));
     }
-    public static int summ(int a, int b) {
+    public static int suma(int a, int b) {
         return a + b;
     }
     public static int minus(int a, int b) {
@@ -26,7 +24,7 @@ public class lesson2 {
     public static int divide(int a, int b) {
         return a / b;
     }
-    public static double summ(double a, double b) {
+    public static double suma(double a, double b) {
         return a + b;
     }
     public static double minus(double a, double b) {
